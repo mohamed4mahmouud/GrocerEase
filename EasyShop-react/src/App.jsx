644b5d86c92962e26f7383bd253aaa1aa324812d
Nourhanshawky <nor.einy2001@gmail.com>
2024-03-14 00:56:59 +0200

    Add routing

--- EasyShop-react/src/App.jsx
+++ EasyShop-react/src/App.jsx
@@ -1,10 +1,33 @@
 import "./App.css";
-import { SignUp } from "./Components/Register/SignUp";
+import Cart from "./Components/Cart/Cart";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
+import Home from "./Components/Home/Home";
+import Layout from "./Components/Layout/Layout";
+import Login from "./Components/Login/Login";
+import Profile from "./Components/Profile/Profile";
+import  {SignUp}  from "./Components/Register/SignUp";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
+let routers = createBrowserRouter([
+{path : '', element:<Layout/> , children:[
+    {index: true, element: <Home/>},
+    {path: 'register', element: <SignUp/>},
+    {path: 'login', element: <Login/>},
+    {path: 'profile', element: <Profile/>},
+    {path: 'cart', element: <Cart/>},
+    {path: '*', element: <ErrorPage/>},
+
+]}
+])
+
+
 
 function App() {
     return (
         <>
-            <SignUp />
+            {/* <SignUp /> */}
+            <RouterProvider router={routers}></RouterProvider>
         </>
     );
 }
