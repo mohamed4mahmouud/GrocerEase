fc1f1111e03496341b4290cc09deecf1101d1065
mohamed4mahmouud <mohamedalamrawy55@gmail.com>
2024-03-13 23:17:11 +0200

    register validation by yup

--- EasyShop-react/src/Components/Register/SignUp.jsx
+++ EasyShop-react/src/Components/Register/SignUp.jsx
@@ -1,20 +1,44 @@
 import { Fragment } from "react";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const SignUp = () => {
+    const phoneRegExp =
+        /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+    let validateSchema = Yup.object({
+        name: Yup.string()
+            .min(3, "Name must be at least 3 characters")
+            .max(15, "Name must be at most 15 characters")
+            .required("name is required"),
+        email: Yup.string()
+            .email("Invalid email address")
+            .required("email is required"),
+        phone: Yup.string()
+            .matches(phoneRegExp, "Phone number is invalid")
+            .required("phone is required"),
+        password: Yup.string()
+            .min(8, "Password must be at least 8 characters")
+            .required("password is required"),
+        confirmPassword: Yup.string()
+            .oneOf([Yup.ref("password")], "Passwords must match")
+            .required("Please confirm your password"),
+    });
+
     function registerSubmit(values) {
         console.log(values);
     }
 
     let formik = useFormik({
         initialValues: {
-            fullName: "",
+            name: "",
             email: "",
             phone: "",
             address: "",
             password: "",
             confirmPassword: "",
         },
+        validationSchema: validateSchema,
         onSubmit: registerSubmit,
     });
 
@@ -23,16 +47,15 @@ export const SignUp = () => {
             <div className="w-75 mx-auto py-5">
                 <h3>Register Now</h3>
                 <form onSubmit={formik.handleSubmit}>
-                    <label htmlFor="fullName">Full Name</label>
+                    <label htmlFor="name">Full Name</label>
                     <input
                         type="text"
                         className="form-control mb-2"
-                        name="fullName"
-                        id="fullName"
-                        value={formik.values.fullName}
+                        name="name"
+                        id="name"
+                        value={formik.values.name}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        required
                     />
                     <label htmlFor="email">Email</label>
                     <input
@@ -43,7 +66,6 @@ export const SignUp = () => {
                         value={formik.values.email}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        required
                     />
                     <label htmlFor="phone">Phone</label>
                     <input
@@ -54,7 +76,6 @@ export const SignUp = () => {
                         value={formik.values.phone}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        required
                     />
                     <label htmlFor="address">Address</label>
                     <input
@@ -65,7 +86,6 @@ export const SignUp = () => {
                         value={formik.values.address}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        required
                     />
                     <label htmlFor="password">Password</label>
                     <input
@@ -76,7 +96,6 @@ export const SignUp = () => {
                         value={formik.values.password}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        required
                     />
                     <label htmlFor="confirmPassword"> Confirm Password</label>
                     <input
@@ -87,7 +106,6 @@ export const SignUp = () => {
                         value={formik.values.confirmPassword}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        required
                     />
                     <button
                         className="btn btn-primary text-white mt-2"
