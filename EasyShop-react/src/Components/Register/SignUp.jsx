00446a9d98863cca4ddf2547bbb45a578e824101
AhmedMeligy <ahmed.tarek540540@gmail.com>
2024-03-14 16:04:45 +0200

    resolve conflict

--- EasyShop-react/src/Components/Register/SignUp.jsx
+++ EasyShop-react/src/Components/Register/SignUp.jsx
@@ -1,35 +1,64 @@
 import { Fragment } from "react";
 import { useFormik } from "formik";
-import style from "./SignUp.module.css"
+import style from "./SignUp.module.css";
+import * as Yup from "yup";
 
 export const SignUp = () => {
+    const phoneRegExp =
+        /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+    let validateSchema = Yup.object({
+        name: Yup.string()
+            .min(3, "Name must be at least 3 characters")
+            .max(50, "Name must be at most 50 characters")
+            .required("name is required"),
+        email: Yup.string()
+            .email("Invalid email address")
+            .required("email is required"),
+        phone: Yup.string()
+            .matches(phoneRegExp, "Phone number is invalid")
+            .required("phone is required"),
+        password: Yup.string()
+            .min(8, "Password must be at least 8 characters")
+            .required("password is required"),
+        confirmPassword: Yup.string()
+            .oneOf([Yup.ref("password")], "Passwords must match")
+            .required("Please confirm your password"),
+    });
+
     function registerSubmit(values) {
         console.log(values);
     }
 
     let formik = useFormik({
         initialValues: {
-            fullName: "",
+            name: "",
             email: "",
             phone: "",
             address: "",
             password: "",
             confirmPassword: "",
         },
+        validationSchema: validateSchema,
         onSubmit: registerSubmit,
     });
 
     return (
         <Fragment>
-            
             <div className={`${style.wrapper} col-md-4 shadow rounded mt-4`}>
                 <div className="container ">
                     <div className="d-flex justify-content-center align-items-center">
                         <div className="mt-3">
-                            <h3 className={`${style.colorPrim} mr-3`}>Talabatk Eh</h3>
+                            <h3 className={`${style.colorPrim} mr-3`}>
+                                Talabatk Eh
+                            </h3>
                         </div>
                         <div className="ms-2">
-                            <img src="../../images/logo.png" alt="" height={70} />
+                            <img
+                                src="../../images/logo.png"
+                                alt=""
+                                height={70}
+                            />
                         </div>
                     </div>
                     <div className="ms-5">
@@ -40,101 +69,139 @@ export const SignUp = () => {
                                 Join Us :)
                             </p>
                         </div>
-                        <div >
-                            <h1 className={`${style.colorPrim} mb-4`}>Register</h1>
+                        <div>
+                            <h1 className={`${style.colorPrim} mb-4`}>
+                                Register
+                            </h1>
                         </div>
-                    <form onSubmit={formik.handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="fullName">Full Name</label>
-                            <input
-                                type="text"
-                                className={`${style.customWidth90} form-control  mb-2`}
-                                id="fullName"
-                                name="fullName"
-                                placeholder="Enter your full name"
-                                value={formik.values.fullName}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                className={`${style.customWidth90} form-control  mb-2`}
-                                id="email"
-                                name="email"
-                                placeholder="Enter email"
-                                value={formik.values.email}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="phoneNumber">Phone Number</label>
-                            <input
-                                type="tel"
-                                className={`${style.customWidth90} form-control  mb-2`}
-                                id="phoneNumber"
-                                name="phoneNumber"
-                                placeholder="Enter phone number"
-                                value={formik.values.phone}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="phoneNumber">Address</label>
-                            <input
-                                type="tel"
-                                className={`${style.customWidth90} form-control  mb-2`}
-                                id="address"
-                                name="address"
-                                placeholder="Enter your adress"
-                                value={formik.values.address}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input
-                                type="password"
-                                className={`${style.customWidth90} form-control  mb-2`}
-                                id="password"
-                                name="password"
-                                placeholder="Enter your Password"
-                                value={formik.values.password}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="confirmPassword">Confirm Password</label>
-                            <input
-                                type="password"
-                                className={`${style.customWidth90} form-control  mb-2`}
-                                id="confirmPassword"
-                                name="confirmPassword"
-                                placeholder="Confirm your Password"
-                                value={formik.values.confirmPassword}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                required
-                            />
-                        </div>
-                        <button
-                            type="submit"
-                            className={`${style.customWidth90} btn btn-primary  mt-2 mb-2`}
-                        >
-                            REGISTER &#8594;
-                        </button>
-                    </form>
+                        <form onSubmit={formik.handleSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="fullName">Full Name</label>
+                                <input
+                                    type="text"
+                                    className={`${style.customWidth90} form-control  mb-2`}
+                                    id="fullName"
+                                    name="fullName"
+                                    placeholder="Enter your full name"
+                                    value={formik.values.fullName}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    required
+                                />
+                                {formik.errors.name && formik.touched.name ? (
+                                    <div className="alert alert-danger p-2 mt-2">
+                                        {formik.errors.name}
+                                    </div>
+                                ) : null}
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="email"
+                                    className={`${style.customWidth90} form-control  mb-2`}
+                                    id="email"
+                                    name="email"
+                                    placeholder="Enter email"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    required
+                                />
+                                {formik.errors.email && formik.touched.email ? (
+                                    <div className="alert alert-danger p-2 mt-2">
+                                        {formik.errors.email}
+                                    </div>
+                                ) : null}
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="phone">Phone Number</label>
+                                <input
+                                    type="tel"
+                                    className={`${style.customWidth90} form-control  mb-2`}
+                                    id="phone"
+                                    name="phone"
+                                    placeholder="Enter phone number"
+                                    value={formik.values.phone}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    required
+                                />
+                                {formik.errors.phone && formik.touched.phone ? (
+                                    <div className="alert alert-danger p-2 mt-2">
+                                        {formik.errors.phone}
+                                    </div>
+                                ) : null}
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="phoneNumber">Address</label>
+                                <input
+                                    type="tel"
+                                    className={`${style.customWidth90} form-control  mb-2`}
+                                    id="address"
+                                    name="address"
+                                    placeholder="Enter your adress"
+                                    value={formik.values.address}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    required
+                                />
+                                {formik.errors.address &&
+                                formik.touched.address ? (
+                                    <div className="alert alert-danger p-2 mt-2">
+                                        {formik.errors.address}
+                                    </div>
+                                ) : null}
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input
+                                    type="password"
+                                    className={`${style.customWidth90} form-control  mb-2`}
+                                    id="password"
+                                    name="password"
+                                    placeholder="Enter your Password"
+                                    value={formik.values.password}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    required
+                                />
+                                {formik.errors.password &&
+                                formik.touched.password ? (
+                                    <div className="alert alert-danger p-2 mt-2">
+                                        {formik.errors.password}
+                                    </div>
+                                ) : null}
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="confirmPassword">
+                                    Confirm Password
+                                </label>
+                                <input
+                                    type="password"
+                                    className={`${style.customWidth90} form-control  mb-2`}
+                                    id="confirmPassword"
+                                    name="confirmPassword"
+                                    placeholder="Confirm your Password"
+                                    value={formik.values.confirmPassword}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    required
+                                />
+                                {formik.errors.confirmPassword &&
+                                formik.touched.confirmPassword ? (
+                                    <div className="alert alert-danger p-2 mt-2">
+                                        {formik.errors.confirmPassword}
+                                    </div>
+                                ) : null}
+                            </div>
+                            <button
+                                type="submit"
+                                className={`${style.customWidth90} btn btn-primary  mt-2 mb-2`}
+                                disabled={!(formik.isValid && formik.dirty)}
+                            >
+                                REGISTER &#8594;
+                            </button>
+                        </form>
                     </div>
                     <div className="text-center ">
                         <p>
@@ -146,21 +213,36 @@ export const SignUp = () => {
                                 Login
                             </a>
                         </p>
-                        </div>
+                    </div>
                     <div className="text-center">
                         <div className="container">
                             <div className="row">
                                 <div className="col-md-6 mx-auto">
                                     <div className={style.linethrough}>
                                         <span className={style.line}></span>
-                                        <span className={`${style.word} ${style.colorSec} fw-bold`}>
+                                        <span
+                                            className={`${style.word} ${style.colorSec} fw-bold`}
+                                        >
                                             OR SIGN UP WITH
                                         </span>
                                         <span className={style.line}></span>
                                     </div>
                                     <div className="mt-2 mb-3">
-                                        <a href=""><img src="../../images/google.png" alt="sdgsd" height={30} /></a>
-                                        <a href=""><img src="../../images/facebook.png" alt="sdgsd" height={30} className="ms-4"/></a>
+                                        <a href="">
+                                            <img
+                                                src="../../images/google.png"
+                                                alt="google"
+                                                height={30}
+                                            />
+                                        </a>
+                                        <a href="">
+                                            <img
+                                                src="../../images/facebook.png"
+                                                alt="facebook"
+                                                height={30}
+                                                className="ms-4"
+                                            />
+                                        </a>
                                     </div>
                                 </div>
                             </div>
@@ -169,16 +251,11 @@ export const SignUp = () => {
                 </div>
             </div>
             <div className={`${style.footerLine} mt-4`}>
-                <span>
-                    .
-                </span>
+                <span>.</span>
             </div>
             <div className={style.image}>
-                <img src="../../images/reg.png" alt="ffffff" />
+                <img src="../../images/reg.png" alt="woman_pic" />
             </div>
-            
-            
         </Fragment>
     );
 };
-
