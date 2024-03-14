51d363a9cb2f6e02423e69abfc49fc0a1ad7e2d0
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2024-03-14 06:07:06 +0200

    implement footer component

--- EasyShop-react/src/Components/Navbar/Navbar.jsx
+++ EasyShop-react/src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   return <>
   <nav className="navbar navbar-expand-lg bgnavbar">
   <div className="container-fluid">
-    <Link className="navbar-brand bgnavbar fw-bold" to="#"><img src={logo} width={40} className='mb-2'/> Talabtk Eh</Link>
+    <Link className="navbar-brand bgnavbar fw-bold" to="#"><img src={logo} width={40} className='mb-2'/> Talabatk Eh</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
