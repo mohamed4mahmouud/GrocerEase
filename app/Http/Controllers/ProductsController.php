5cef58beb9c3d8b4d240e4e42c9b47514dde1919
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2024-03-15 15:02:02 +0200

    create cruds for products

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,8 +9,68 @@ use Illuminate\Http\Request;
 class ProductsController extends Controller
 {
     use GeneralTrait;
-    public function getAllProducts(){
-        $products=Product::all();
-        return $this->returnData('products',$products,'Success');
+
+    public function getAllProducts()
+    {
+        $products = Product::all();
+        return $this->returnData('products', $products, 'Success');
+    }
+    public function getProductById($id)
+    {
+        $products = Product::find($id);
+        return $this->returnData('products', $products, 'Success');
+    }
+    public function create(Request $request)
+    {
+        try {
+            if ($request->user()) {
+                $products = new Product();
+
+                $products->title = $request->input('title');
+                $products->description = $request->input('description');
+                $products->price = $request->input('price');
+                $products->quantity = $request->input('quantity');
+                $products->user_id = $request->user()->id;
+                $products->save();
+
+                return $this->returnData('products', $products, 'Success');
+            }
+        } catch (\Exception $e) {
+            return $this->returnError(500, 'Error occurred while creating the product.');
+        }
+    }
+    public function updateById(Request $request, $id)
+    {
+
+        $products = Product::find($id);
+        if ($request->user()->id !== $products->user_id) {
+            return $this->returnError(403, 'Unauthorized');
+        }
+        try {
+            $products->title = $request->input('title');
+            $products->description = $request->input('description');
+            $products->price = $request->input('price');
+            $products->quantity = $request->input('quantity');
+
+            $products->save();
+            return $this->returnData('products', $products, 'Success');
+        } catch (\Exception $e) {
+            return $this->returnError(500, 'Error occurred while updating the product.');
+        }
+    }
+
+    public function deleteproduct(Request $request, $id)
+    {
+        $products = Product::find($id);
+
+        if ($request->user()->id !== $products->user_id) {
+            return $this->returnError(403, 'Unauthorized');
+        }
+        try {
+            $products->delete();
+            return $this->returnData('products', $products, 'Success');
+        } catch(\Exception $e) {
+            return $this->returnError(500, 'Error occurred while deleting the product.');
+        } 
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,13 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
-    protected $fillable=[
-        
+    protected $table = 'products';
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'quantity',
+        'user_id'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
