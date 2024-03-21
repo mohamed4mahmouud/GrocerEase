2d0a63cfe1b46de5d10b6748bc2dd65768497792
mohamed4mahmouud <mohamedalamrawy55@gmail.com>
2024-03-21 17:32:06 +0200

    Reviews Crud opertaions

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -17,7 +17,8 @@ class ReviewController extends Controller
     //@route post api/products/{product}/reviews
     public function addReview(ReviewRequest $request, Product $product)
     {
-        // Check if the user has already created a review for this product
+        try {
+             // Check if the user has already created a review for this product
         $review = Review::where('user_id',$request->user()->id)->where('product_id', $product->id)->exists();
 
         if($review){
@@ -31,6 +32,9 @@ class ReviewController extends Controller
             'product_id' => $product->id,
         ]);
         return $this->returnData('review',$newReview,'success');
+        } catch (\Exception $e) {
+            return $this->returnError(500, 'Error occurred while create Review');
+        }
     }
 
     //@desc get all reviews
@@ -38,13 +42,18 @@ class ReviewController extends Controller
     //@route get api/products/{product}/reviews
     public function getAllReviews(Product $product)
     {
-        $reviews = Review::where('product_id', $product->id)->get();
+        try {
+            $reviews = Review::where('product_id', $product->id)->get();
 
-        if(!$reviews){
-            return $this->returnError(404 , 'No reviews for this product');
+            if(!$reviews){
+                return $this->returnError(404 , 'No reviews for this product');
+            }
+
+            return $this->returnData('reviews' , $reviews , 'success');
+        } catch (\Exception $e) {
+            return $this->returnError(500, 'Error occurred while get Reviews');
         }
 
-        return $this->returnData('reviews' , $reviews , 'success');
     }
 
      //@desc update review
@@ -52,23 +61,28 @@ class ReviewController extends Controller
     //@route put api/products/{product}/reviews
     public function updateReview(ReviewRequest $request , Product $product , string $id)
     {
-        $review = Review::find($id);
+        try {
+            $review = Review::find($id);
 
-        if(!$review){
-            return $this->returnError(404 , 'No review for this id');
+            if(!$review){
+                return $this->returnError(404 , 'No review for this id');
+            }
+
+            // Check if the current user is the owner of the review
+            if($review->user_id != $request->user()->id){
+                return $this->returnError(401 , "You are not allowed to update this review");
+            }
+
+            // Update the review with new comment and rating
+            $review->update([
+                'comment'=> $request->comment,
+                'rating' => $request->rate
+            ]);
+            return $this->returnData('review' , $review , 'Updated');
+        } catch (\Exception $e) {
+            return $this->returnError(500, 'Error occurred while update Review');
         }
 
-        // Check if the current user is the owner of the review
-        if($review->user_id != $request->user()->id){
-            return $this->returnError(401 , "You are not allowed to update this review");
-        }
-
-        // Update the review with new comment and rating
-        $review->update([
-            'comment'=> $request->comment,
-            'rating' => $request->rate
-        ]);
-        return $this->returnData('review' , $review , 'Updated');
     }
 
     //@desc delete review
@@ -76,7 +90,8 @@ class ReviewController extends Controller
     //@route delete api/products/{product}/reviews
     public function deleteReview(Request $request ,Product $product, string $id )
     {
-        $review = Review::find($id);
+        try {
+            $review = Review::find($id);
 
         if(!$review){
             return $this->returnError(404 , 'No review for this id');
@@ -89,5 +104,9 @@ class ReviewController extends Controller
 
         $review->delete();
         return $this->returnSuccessMessage('Review Deleted');
+        } catch (\Exception $e) {
+            return $this->returnError(500, 'Error occurred while delete Review');
+        }
+
     }
 }
