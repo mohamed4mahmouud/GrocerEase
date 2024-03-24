939fc798cd15c6cfdee17e00376a3fbfd23f78a0
Nourhanshawky <nor.einy2001@gmail.com>
2024-03-24 19:50:08 +0200

    add error page

--- EasyShop-react/src/Components/ErrorPage/ErrorPage.jsx
+++ EasyShop-react/src/Components/ErrorPage/ErrorPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <img src="../../images/errorPage.jpg" alt="" width={600} />
+    </div>
   )
 }
